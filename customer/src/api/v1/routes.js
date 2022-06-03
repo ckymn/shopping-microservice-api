5ffd1ad359873885fe4e4a2b9277ecbd77f58eae
ckymn <cokyamanmuhammet@gmail.com>
2022-06-03 15:34:55 +0300

    customer validation integrated

--- customer/src/api/v1/routes.js
+++ customer/src/api/v1/routes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const { validate } = require("./middleware");
+const { createValidation } = require("./validations/customer.validation");
+
 const {
   createController,
   listController,
 } = require("./controllers/customer.controller");
 
-router.post("/customer/signup", createController);
 router.get("/customer/profile", listController);
+router.post("/customer/signup", validate(createValidation), createController);
 
 module.exports = router;
---
+++ customer/src/api/v1/validations/customer.validation.js
@@ -0,0 +1,19 @@
+const Joi = require("joi");
+
+const createValidation = Joi.object({
+  email: Joi.string().required().min(5),
+  password: Joi.string().required().min(3),
+  salt: Joi.string(),
+  phone: Joi.string(),
+  address: Joi.string(),
+  cart: Joi.array().items({
+    product: Joi.string(),
+    unit: Joi.number(),
+  }),
+  wishlist: Joi.array(),
+  orders: Joi.array(),
+});
+
+module.exports = {
+  createValidation,
+};
