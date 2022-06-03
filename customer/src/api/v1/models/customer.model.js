69c3ac5c0625fc5287ae1e4eaac59ae3aae985b7
ckymn <cokyamanmuhammet@gmail.com>
2022-06-03 16:54:25 +0300

    customer looger integrated

--- customer/src/api/v1/models/customer.model.js
+++ customer/src/api/v1/models/customer.model.js
@@ -1,43 +1,51 @@
 const { Schema, model } = require("mongoose");
+const logger = require("../scripts/logger/customer.log");
 
-module.exports = customerSchema = new model(
-  "customer",
-  new Schema(
-    {
-      email: String,
-      password: String,
-      salt: String,
-      phone: String,
-      address: [{ type: Schema.Types.ObjectId, ref: "address", require: true }],
-      cart: [
-        {
-          product: {
-            type: Schema.Types.ObjectId,
-            ref: "product",
-            require: true,
-          },
-          unit: { type: Number, require: true },
-        },
-      ],
-      wishlist: [
-        {
+const customerSchema = new Schema(
+  {
+    email: String,
+    password: String,
+    salt: String,
+    phone: String,
+    address: [{ type: Schema.Types.ObjectId, ref: "address", require: true }],
+    cart: [
+      {
+        product: {
           type: Schema.Types.ObjectId,
           ref: "product",
           require: true,
         },
-      ],
-      orders: [{ type: Schema.Types.ObjectId, ref: "order", require: true }],
-    },
-    {
-      toJSON: {
-        transform(doc, ret) {
-          delete ret.password;
-          delete ret.salt;
-          delete ret.__v;
-        },
+        unit: { type: Number, require: true },
       },
-      versionKey: false,
-      timestamps: true,
-    }
-  )
+    ],
+    wishlist: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "product",
+        require: true,
+      },
+    ],
+    orders: [{ type: Schema.Types.ObjectId, ref: "order", require: true }],
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        delete ret.password;
+        delete ret.salt;
+        delete ret.__v;
+      },
+    },
+    versionKey: false,
+    timestamps: true,
+  }
 );
+
+customerSchema.post("save", (doc, next) => {
+  logger.log({
+    level: "info",
+    message: doc,
+  });
+  next();
+});
+
+module.exports = model("customer", customerSchema);
