b551b45d32a590ee7d8b304531be02e5cffa2db1
ckymn <cokyamanmuhammet@gmail.com>
2022-06-06 14:21:14 +0300

    docker-compose was added

--- customer/src/api/index.js
+++ customer/src/api/index.js
@@ -1,10 +1,10 @@
-const { connection, server } = require("./v1/utils");
+const { connection, createServer } = require("./v1/utils");
 const config = require("./v1/config");
 const Routes = require("./v1/routes");
 
 config();
 
-const app = server();
+const app = createServer();
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST;
 
--- customer/src/api/v1/utils/index.js
+++ customer/src/api/v1/utils/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   errorHandler: require("./errorHandler.utils"),
-  server: require("./server.utils"),
+  createServer: require("./server.utils"),
   connection: require("./connection.utils"),
   passwordToHash: require("./helper.utils"),
 };
